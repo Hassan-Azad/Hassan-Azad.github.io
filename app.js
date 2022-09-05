9bb27a27ce4ac23899a35be15d17769989336aac
Hassan-Azad <zainsatti1901@gmail.com>
2022-09-05 14:16:33 +0500

    Front end web Development

--- app.js
+++ app.js
@@ -26,3 +26,32 @@ navLink.forEach((link) =>
     ul.classList.remove("show");
   })
 );
+
+function reveal(){
+  var reveals = document.querySelectorAll(".icon");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
+
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("inner").style.opacity = "0.2";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("inner").style.opacity = "2";
+}
